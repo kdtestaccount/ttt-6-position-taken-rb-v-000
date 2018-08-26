cec04a705fab34b8401e339f18f39d6f55fc37a4
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-26 16:51:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == "" || " " || "   "
+  if board[index] == "" || " " || "   " || nil
     false
-  elsif board[index] == "X" || "O"
-    true
   else
-    nil
+    true
   end
 end
